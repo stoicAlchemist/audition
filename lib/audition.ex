c0732148dcb67b8c70e3b3782120c754c41faf8a
Brian Martinez <1622554+stoicAlchemist@users.noreply.github.com>
2022-10-29 01:07:41 -0500

    init(Mix): [NA] First commit

---
+++ lib/audition.ex
@@ -0,0 +1,18 @@
+defmodule Audition do
+  @moduledoc """
+  Documentation for `Audition`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Audition.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/audition_test.exs
@@ -0,0 +1,8 @@
+defmodule AuditionTest do
+  use ExUnit.Case
+  doctest Audition
+
+  test "greets the world" do
+    assert Audition.hello() == :world
+  end
+end
